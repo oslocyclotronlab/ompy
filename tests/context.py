d61300b09b39f56b54a44f85053576d2ac433426
Jørgen Eriksson Midtbø <jorgenem@gmail.com>
2019-01-09 10:24:22 +0100

    Added tests

---
+++ tests/context.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Borrowed from https://github.com/kennethreitz/samplemod/
+
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import oslo_method_python
+
