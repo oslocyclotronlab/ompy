eaefb8acd67ab4e3c703e49f1573efaf00008ed7
Jørgen Eriksson Midtbø <jorgenem@gmail.com>
2019-01-11 11:32:58 +0100

    Working on Cython implementation of general rebin functino

--- oslo_method_python/rebin.pyx
+++ oslo_method_python/rebin.pyx
@@ -2,47 +2,78 @@
 # Implemented using Cython, with inspiration from here:
 # https://cython.readthedocs.io/en/latest/src/userguide/numpy_tutorial.html
 import numpy as np
+cimport cython
 
 
 # We now need to fix a datatype for our arrays. I've used the variable
 # DTYPE for this, which is assigned to the usual NumPy runtime
 # type info object.
-DTYPE = np.intc
+DTYPE = np.float64
 
 
-def rebin_cython(counts_in, E_array_in, E_array_out):
+cdef double calc_overlap(double Ein_l, double Ein_h,
+                         double Eout_l, double Eout_h):
+    overlap = max(0,
+                  min(Eout_h, Ein_h)
+                  - max(Eout_l, Ein_l)
+                  )
+    return overlap
+
+
+@cython.boundscheck(False)  # Deactivate bounds checking
+@cython.wraparound(False)   # Deactivate negative indexing.
+@cython.cdivision(True)
+def rebin_cython(double[:] counts_in, double[:] E_array_in,
+                 double[:] E_array_out):
     """Rebin an array of counts from binning E_array_in to binning E_array_out
     """
 
-    assert counts_in.dtype == DTYPE
-    assert E_array_in.dtype == DTYPE
-    assert E_array_out.dtype == DTYPE
+    # assert counts_in.dtype == DTYPE
+    # assert E_array_in.dtype == DTYPE
+    # assert E_array_out.dtype == DTYPE
 
-    cdef int Nin
-    cdef int Nout
-    cdef double a1_in
+    cdef int Nin, Nout
+    cdef int jmin, jmax  # To select subset in inner loop below
+    cdef double a0_in, a1_in, a0_out, a1_out
 
+    # Get calibration coefficients and number of elements from array:
     Nin = E_array_in.shape[0]
-    Nout = E_array_in.shape[1]
+    a0_in, a1_in = E_array_in[0], E_array_in[1]-E_array_in[0]
+    Nout = E_array_out.shape[0]
+    a0_out, a1_out = E_array_out[0], E_array_out[1]-E_array_out[0]
+
     a1_in = E_array_in[1]-E_array_in[0]
 
     # For the overlap calculation, it is convenient to have energy arrays with
     # one extra bin (the upper edge of the last bin):
-    E_array_in_ext = np.append(E_array_in, E_array_in[-1]+a1_in)
-    E_array_out_ext = np.append(E_array_out,
-                                E_array_out[-1]
-                                + (E_array_out[1]-E_array_out[0]))
+    # E_array_in_ext = np.append(E_array_in, E_array_in[Nin]+a1_in)
+    # E_array_out_ext = np.append(E_array_out,
+                                # E_array_out[Nout]
+                                # + (E_array_out[1]-E_array_out[0]))
 
     # Allocate rebinned array to fill:
     counts_out = np.zeros(Nout, dtype=DTYPE)
+    cdef double[:] counts_out_view = counts_out
+    cdef int i, j
+    cdef double Eout_i, Ein_j
     for i in range(Nout):
-        for j in range(Nin):
+        # for j in range(Nin):
+        # Can we make it faster by looping j over a subset? What is a
+        # sufficient subset? Yes!
+        jmin = max(0, <int>((a0_out + a1_out*(i-1) - a0_in)/a1_in))
+        jmax = min(Nin-1, <int>((a0_out + a1_out*(i+1) - a0_in)/a1_in))
+        # Calculate the bin edge energies manually for speed:
+        Eout_i = a0_in + a1_in*i
+        for j in range(jmin, jmax+1):
             # Calculate proportionality factor based on current overlap:
-            overlap = max(0, (
-                                min(E_array_out[i+1], E_array_in[j+1])
-                                - max(E_array_out[i], E_array_in[j]))
-                          )
-            counts_out[i] += counts_in[j] * overlap / a1_in
+            Ein_j = a0_out + a1_out*j
+            overlap = calc_overlap(Ein_j, Ein_j+a1_in,
+                                   Eout_i, Eout_i+a1_out)
+            # overlap = max(0, (
+            #                     min(E_array_out_ext[i+1], E_array_in_ext[j+1])
+            #                     - max(E_array_out_ext[i], E_array_in_ext[j]))
+            #               )
+            counts_out_view[i] += counts_in[j] * overlap / a1_in
 
     return counts_out
 
@@ -50,7 +81,7 @@ def rebin_cython(counts_in, E_array_in, E_array_out):
 # Look at this for inspiration, delete after:
 def rebin_python(counts_in, Ein_array, Eout_array):
     """
-    Rebins, i.e. redistributes the numbers from vector counts 
+    Rebins, i.e. redistributes the numbers from vector counts
     (which is assumed to have calibration given by Ein_array) 
     into a new vector, which is returned. 
     The total number of counts is preserved.
@@ -83,7 +114,12 @@ def rebin_python(counts_in, Ein_array, Eout_array):
     counts_out = np.zeros(Nout)
     # Loop over all indices in both arrays. Maybe this can be speeded up?
     for i in range(Nout):
-        for j in range(Nin):
+        # for j in range(Nin):
+        # Can we make it faster by looping j over a subset? What is a
+        # sufficient subset?
+        jmin = max(0, int((a0_out + a1_out*(i-1) - a0_in)/a1_in))
+        jmax = min(Nin-1, int((a0_out + a1_out*(i+1) - a0_in)/a1_in))
+        for j in range(jmin, jmax+1):
             # Calculate proportionality factor based on current overlap:
             overlap = np.minimum(Eout_array[i+1], Ein_array[j+1]) - np.maximum(Eout_array[i], Ein_array[j])
             overlap = overlap if overlap > 0 else 0
---
+++ tests/rebin_test_and_compare.py
@@ -0,0 +1,56 @@
+# Compare rebin functions between Python and Cython
+
+from context import oslo_method_python as om
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+
+N = 10
+counts_in = np.random.uniform(size=N)
+print("counts_in.dtype =", counts_in.dtype, flush=True)
+E_array_in = np.linspace(0, 5000, N)
+
+# E_array_out = np.linspace(200, 5000, 300)
+E_array_out = np.linspace(0, 5000, int(3*N))
+
+t1 = time.time()
+counts_out_python = om.rebin_python(counts_in, E_array_in, E_array_out)
+t2 = time.time()
+print("python time =", t2-t1)
+
+t1 = time.time()
+counts_out_cython = om.rebin_cython(counts_in, E_array_in, E_array_out)
+t2 = time.time()
+print("cython time =", t2-t1)
+
+# Plot them all
+f, ax = plt.subplots(1, 1)
+ax.step(E_array_in, counts_in, where="post", label="in")
+ax.step(E_array_out, counts_out_python, where="mid", label="out (python)")
+ax.step(E_array_out, counts_out_cython, where="post", label="out (cython)")
+
+ax.legend()
+plt.show()
+
+
+# Related test: What is sufficient to get complete overlap between 
+# initial and final bins?
+# Get calibration coefficients and number of elements from array:
+Nin = len(E_array_in)
+a0_in, a1_in = E_array_in[0], E_array_in[1]-E_array_in[0]
+Nout = len(E_array_out)
+a0_out, a1_out = E_array_out[0], E_array_out[1]-E_array_out[0]
+
+# for i in range(Nout):
+#     jmin = max(0, int((a0_out + a1_out*(i-1) - a0_in)/a1_in))
+#     jmax = min(Nin-1, int((a0_out + a1_out*(i+1) - a0_in)/a1_in))
+#     print("E_array_out[i] =", E_array_out[i])
+#     for j in range(jmin, jmax+1):
+#         print("E_array_in[j] =", E_array_in[j])
+#         if E_array_in[jmin] > E_array_out[i]:
+#             print("E_array_in[jmin] > E_array_out[i]")
+#         if E_array_in[jmax]+a1_in < E_array_out[i]+a1_out:
+#             print("E_array_in[jmax]+a1_in = E_array_in[jmax+1] < E_array_out[i]+a1_out=E_array_out[i+1]")
+
+# Conclusion: Looks like it's only a problem on the edges, 
+# and that's always going to be tricky.
