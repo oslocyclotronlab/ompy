5d2212c68452c7260b3cb166602bfd00ee838f2a
Jørgen Eriksson Midtbø <jorgenem@gmail.com>
2019-01-23 16:19:24 +0100

    Started implementing fitting function for rho and T. Realized it's easier to borrow Fabio 's code and implement it myself into my framework, rather than submoduling rhosig.py

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 
-setup(name='Rebin function',
+# build me using
+# python setup.py build_ext --inplace
+
+setup(name='rebin',
       ext_modules=cythonize("oslo_method_python/rebin.pyx"))
+
+setup(name='fit_rho_T',
+      ext_modules=cythonize("oslo_method_python/fit_rho_T.pyx"))
