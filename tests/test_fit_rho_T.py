eaa20fdda4570b649cfd1ff8b01668bbbc6b6701
Jørgen Eriksson Midtbø <jorgenem@gmail.com>
2019-01-24 15:48:17 +0100

    Implemented a working version of fit_rho_T. So far it doesn't use Cython, but I try to do most of the heavy lifting in numpy by repeating and multiplying arrays to construct the P_fit matrix. It remains to properly implement Ex and Eg limits for the fit, as well as thorough bugtesting of course.

--- tests/test_fit_rho_T.py
+++ tests/test_fit_rho_T.py
@@ -8,22 +8,35 @@ firstgen = om.Matrix()
 # TODO: Make a test matrix (could be random numbers)
 # to ship with the package, to use for unittesting
 # on unfolding, firstgen method and here.
-# Should probably save all steps of the analysis (raw, 
+# Should probably save all steps of the analysis (raw,
 # unfolded, firstgen) as separate matrices.
-firstgen.load("../firstgen-28Si.m")
+# firstgen.load("../firstgen-28Si.m")
+# Testing with a proper firstgen for a heavy nucleus (development):
+firstgen.load("/home/jorgenem/MEGA/doktorgrad/oslometode_usikkerhetspropagering/Dy164/data/fg")
 firstgen.std = np.sqrt(firstgen.matrix)  # In lieu of something better
+# print("firstgen.std =", firstgen.std)
 
-calib_out = {"a0": -20, "a1": 30}
+calib_out = {"a0": 0, "a1": 120}
 Ex_min = firstgen.E0_array[0]
-Ex_max = firstgen.E0_array[-1]
+Ex_max = 8000
 Eg_min = Ex_min
+print("Ex_min =", Ex_min)
+print("Ex_max =", Ex_max)
+print("Eg_min =", Eg_min)
 
 rho, T = om.fit_rho_T(firstgen, calib_out,
                       Ex_min, Ex_max, Eg_min)
 
 
 # Plot
-f, (axrho, axT) = plt.subplots(1, 2)
-rho.plot(ax=axrho)
-T.plot(ax=axT)
+f, (axrho, axT, axgsf) = plt.subplots(1, 3)
+axrho.set_title("rho")
+rho.plot(ax=axrho, yscale="log")
+axT.set_title("T")
+T.plot(ax=axT, yscale="log")
+# Also plot gsf, i.e. T/Eg^3
+gsf = om.div0(T.vector, T.E_array**3)
+axgsf.plot(T.E_array, gsf)
+axgsf.set_title("gsf")
+axgsf.set_yscale("log")
 plt.show()
