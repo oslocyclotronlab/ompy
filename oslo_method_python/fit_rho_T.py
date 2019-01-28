Implemented a masking array in the chisquare evaluation. There still seems to be something wrong with how rho(Ex) is evaluated, since the fit should change its values all the way down to zero

oslo_method_python/fit_rho_T.py
@@ -5,8 +5,14 @@
 import numpy as np
 from .library import *
 from .rebin import *
+from .constants import *
 from scipy.optimize import minimize
 import copy
+
+# Define constants from constants.py as global vars
+global DE_PARTICLE
+global DE_GAMMA_1MEV
+global DE_GAMMA_8MEV
 
 
 def fit_rho_T(firstgen_in, bin_width_out,
@@ -32,7 +38,6 @@ def fit_rho_T(firstgen_in, bin_width_out,
         If firstgen.std=None, it could call a function to estimate the standard
         deviation using the rhosigchi techniques.
     """
-
     # Cut the firstgen.matrix and firstgen.std to shape [Ex_min:Ex_max,
     # Eg_min:Eg_max].
     # Protect the input matrix:
@@ -76,6 +81,7 @@ def fit_rho_T(firstgen_in, bin_width_out,
             calib_firstgen["a11"] == calib_out["a1"]
            )
 
+
     # Normalize the firstgen matrix for each Ex bin:
     P_exp = div0(firstgen.matrix, firstgen.matrix.sum(axis=1))
     P_err = firstgen.std
@@ -104,9 +110,30 @@ def fit_rho_T(firstgen_in, bin_width_out,
     # res = chisquare_1D(p0, firstgen.matrix, firstgen.std, E_array)
     # print("chisq(p0) = ", res)
 
+    # Set up the masking array which defines the area to include in the
+    # chisquare fit:
+    # TODO move out to its own function
+    masking_array = make_masking_array(shape=firstgen.matrix.shape,
+                                       Ex_min=Ex_min,
+                                       Ex_max=Ex_max,
+                                       Eg_min=Eg_min,
+                                       E_array=E_array
+                                       )
+
+    # # DEBUG
+    # from matplotlib.colors import LogNorm
+    # fdebug, axdebug = plt.subplots(1, 1)
+    # axdebug.pcolormesh(firstgen.E1_array, firstgen.E0_array,
+    #                    firstgen.matrix,
+    #                    norm=LogNorm())
+    # plt.show()
+    # import sys
+    # sys.exit(0)
+    # # END DEBUG
+
     res = minimize(chisquare_1D, x0=p0,
                    args=(firstgen.matrix, firstgen.std,
-                         E_array),
+                         E_array, masking_array),
                    method="Powell",
                    options={'disp': True})
 
@@ -120,7 +147,6 @@ def fit_rho_T(firstgen_in, bin_width_out,
     # 2. Normalize firstgen (and stddev?)
     # 3. Write function that makes probability matrix from rho and T
     # 4. Write chisquare function. Interface to standard minimizers.
-
 
     # Dummy values to return for testing:
     rho = Vector(rho_array, E_array)
@@ -144,7 +170,7 @@ def chisquare_1D(x, *args):
 
     """
 
-    P_exp, P_err, E_array = args
+    P_exp, P_err, E_array, masking_array = args
     # Are the following things needed?
     # P_exp = np.asarray(P_exp)
     # P_exp_err = np.asarray(P_exp_err)
@@ -158,16 +184,22 @@ def chisquare_1D(x, *args):
 
     P_fit = construct_P(rho_array, T_array, E_array)
 
-    return chisquare(rho_array, T_array, P_exp, P_err, P_fit, E_array)
+    return chisquare(rho_array, T_array, P_exp, P_err, P_fit, E_array,
+                     masking_array)
 
 
-def chisquare(rho, T, P_exp, P_err, P_fit, E_array):
+def chisquare(rho, T, P_exp, P_err, P_fit, E_array,
+              masking_array):
     """ Chi^2 between experimental and fitted first genration matrix"""
     # if np.any(rho<0) or np.any(T<0): # hack to implement lower boundary
         # chi2 = 1e20
     # else:
     # chi^2 = (data - fit)^2 / unc.^2, where unc.^2 = #cnt for Poisson dist.
-    chi2 = np.sum(div0((P_exp - P_fit)**2, P_err**2))
+    chi2_matrix = div0((P_exp - P_fit)**2, P_err**2)
+    # print("from chisquare(): chi2_matrix.shape =", chi2_matrix.shape)
+    # print("from chisquare(): chi2_matrix[masking_array].shape =",
+          # chi2_matrix[masking_array].shape)
+    chi2 = np.sum(chi2_matrix[masking_array])
     return chi2
 
 
@@ -197,4 +229,49 @@ def construct_P(rho_array, T_array, E_array):
     rho_grid = np.zeros((Nbins, Nbins))
     for j in range(Nbins):
         rho_grid[j:Nbins, j] = rho_array[0:Nbins-j]
+
+    # # DEBUG
+    # # This is neat, it prints P_fit in every
+    # # iteration of the minimization. Gives valuable insight!
+    # fdebug2, axdebug2 = plt.subplots(1, 1)
+    # axdebug2.pcolormesh(E_array, E_array, rho_grid*T_grid)
+    # plt.show()
+    # # END DEBUG
+
     return rho_grid*T_grid
+
+
+def make_masking_array(shape, Ex_min, Ex_max, Eg_min, E_array):
+    """
+    Returns a boolean index matrix of dimension "shape".
+
+    The mask is a trapezoid limited on three sides by Ex_min, Ex_max and
+    Eg_min. The right edge is the diagonal defined by Eg = Ex + dE, where dE
+    is given by the resolution of the detectors.
+    """
+    masking_array = np.zeros(shape, dtype=bool)
+    i_Ex_min = i_from_E(Ex_min, E_array)
+    i_Ex_max = i_from_E(Ex_max, E_array)
+    i_Eg_min = i_from_E(Eg_min, E_array)
+    # print("i_Ex_min =", i_Ex_min)
+    # print("i_Ex_max =", i_Ex_max)
+    # print("i_Eg_min =", i_Eg_min)
+    for i_Ex in range(i_Ex_min, i_Ex_max+1):
+        # Loop over rows in array and fill with ones up to sliding Eg
+        # threshold
+        Ex = E_array[i_Ex]
+        # Assume constant particle resolution:
+        dE_particle = DE_PARTICLE
+        # Interpolate the gamma resolution linearly:
+        dE_gamma = ((DE_GAMMA_8MEV - DE_GAMMA_1MEV) / (8000 - 1000)
+                    * (Ex - 1000))
+        Eg_max = Ex + np.sqrt(dE_particle**2 + dE_gamma**2)
+        i_Eg_max = i_from_E(Eg_max, E_array)
+        if i_Eg_max < i_Eg_min: 
+            continue
+        # print("Ex = ", Ex, "i_Eg_max =", i_Eg_max)
+        masking_array[i_Ex_min:i_Ex_max, i_Eg_min:i_Eg_max] = 1
+
+    print("masking_array.shape =", masking_array.shape)
+
+    return masking_array