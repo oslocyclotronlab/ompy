69668ae2039dbd8131e00e46b82e7ca0c6f2564d
Jørgen Eriksson Midtbø <jorgenem@gmail.com>
2019-02-04 15:31:43 +0100

    It runs...

--- tests/test_first_generation_method.py
+++ tests/test_first_generation_method.py
@@ -6,22 +6,19 @@ import matplotlib.pyplot as plt
 # Test the first_generation_method() and helper functions
 
 fname_unfolded = "/home/jorgenem/MEGA/doktorgrad/oslometode_usikkerhetspropagering/Dy164/data/alfnaun"
-# Call it through the MatrixAnalysis class for now,
-# but firstgen should be made callable as a standalone function.
-ma = om.MatrixAnalysis()
-
-ma.unfolded.load(fname_unfolded)
+matrix_in = om.Matrix()
+matrix_in.load(fname_unfolded)
 
 f2D, (ax2D1, ax2D2) = plt.subplots(1, 2)
-cbar = ma.unfolded.plot(ax=ax2D1, title="Dy164 unfolded",
+cbar = matrix_in.plot(ax=ax2D1, title="Dy164 unfolded",
                         zscale="log", zmin=1e-3)
 f2D.colorbar(cbar, ax=ax2D1)
 
 # Run first generation method
 Ex_max = 8500
 dE_gamma = 500
-ma.first_generation_method(Ex_max=Ex_max, dE_gamma=dE_gamma)
-ma.firstgen.plot(ax=ax2D2, title="Dy164 first-generation",
+matrix_fg = om.first_generation_method(matrix_in, Ex_max=Ex_max, dE_gamma=dE_gamma)
+matrix_fg.plot(ax=ax2D2, title="Dy164 first-generation",
                         zscale="log")
 f2D.colorbar(cbar, ax=ax2D2)
 
