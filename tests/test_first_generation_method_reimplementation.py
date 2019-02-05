8762434fb1444ca3d6e2f2b6ae1c41b0ad31df73
Jørgen Eriksson Midtbø <jorgenem@gmail.com>
2019-02-05 16:29:25 +0100

    Works much better, but not quite there yet. Need to try Fermi gas inital weights (although this should be irrelevant, otherwise model dependence etc...) and proper area correction alpha. Currently alpha is fixed to 0.85 and this seems to help relative to using alpha=1.

--- tests/test_first_generation_method_reimplementation.py
+++ tests/test_first_generation_method_reimplementation.py
@@ -6,17 +6,28 @@ fname_unfolded = "/home/jorgenem/MEGA/doktorgrad/oslometode_usikkerhetspropageri
 allgen = om.Matrix()
 allgen.load(fname_unfolded)
 
+# Calculate firstgen using om:
 firstgen = om.first_generation_method_reimplementation(allgen)
+
+# Load firstgen calculated with MAMA to compare:
+fname_fg_mama = "/home/jorgenem/MEGA/doktorgrad/oslometode_usikkerhetspropagering/Dy164/data/fg"
+firstgen_mama = om.Matrix()
+firstgen_mama.load(fname_fg_mama)
 
 
 # === Plot them ===
-f, (ax1, ax2) = plt.subplots(1, 2)
+f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
 # All generations
 cbar1 = allgen.plot(ax=ax1, title="all generations matrix", zmin=1e-2)
 f.colorbar(cbar1, ax=ax1)
 # First generation
 cbar2 = firstgen.plot(ax=ax2, title="firstgen reimplementation OM Python",
-                      zmin=1e-2)
+                      zmin=1e-2, zmax=5e3)
 f.colorbar(cbar2, ax=ax2)
+# First generation from MAMA
+cbar3 = firstgen_mama.plot(ax=ax3, title="firstgen MAMA",
+                      zmin=1e-2)
+f.colorbar(cbar3, ax=ax3)
+
 
 plt.show()
