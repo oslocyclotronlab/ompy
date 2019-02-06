2cdff412d396db92fa39a11ec25f39c49254446e
Jørgen Eriksson Midtbø <jorgenem@gmail.com>
2019-02-06 19:12:11 +0100

    Wrote consistent header strings in files

--- oslo_method_python/unfold.py
+++ oslo_method_python/unfold.py
@@ -1,3 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Implementation of the unfolding method
+(Guttormsen et al., Nuclear Instruments and Methods in Physics Research A
+374 (1996))
+---
+
+This file is part of oslo_method_python, a python implementation of the
+Oslo method.
+
+Copyright (C) 2018 Jørgen Eriksson Midtbø
+Oslo Cyclotron Laboratory
+jorgenem [0] gmail.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
 from .library import *
 from .constants import *
 
