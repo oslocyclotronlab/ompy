2848a1e44f50da34dd86e6e97f840b9286792b5e
Erlend Lima <erlendlima@outlook.com>
2019-03-19 12:01:33 +0100

    Complete Ensemble class
    The MatrixAnalysis class is redundant and is pending removal in
    a later change once all necessary details have been taken care of.
    
    The ErrorPropagation class is rewritten into Ensemble. The design of
    Ensemble is for the user to specify the unfolding and first generation methods
    using custom Unfolder and FirstGeneration instances which are given to
    Ensemble as
    
    ```python
        unfolder = Unfolder()
        unfolder.cut_diagonal(min, max)
        firstgen = FirstGeneration()
        firstgen.dEg = 800
        ensemble = Ensemble(raw)
        ensemble.unfolder = unfolder
        ensemble.firstgen = firstgen
        ensemble.generate(number=10)
    ```
    
    This makes it easy and flexible to micromanage the ensemble generation.
    
    Work has begun on the FirstGeneration class, but is not near ready.
    In the meantime, use a lambda to enclose a call to first_generation_method.
    
    Minor fixes as been done all around the code:
     * Matrix now accepts [] for getting and setting Matrix.values
     * Matrix.state can be set by a string which is then converted into a
       MatrixState
     * Adjusted the call signature of some functions for consistency

--- ompy/unfolder.py
+++ ompy/unfolder.py
@@ -33,6 +33,7 @@ from typing import Iterable
 from .library import div0
 from .matrix import Matrix, MatrixState
 from scipy.ndimage import gaussian_filter1d
+from copy import copy
 
 
 LOG = logging.getLogger(__name__)
@@ -70,7 +71,7 @@ class Unfolder:
         minimum_iterations int:
 
     """
-    def __init__(self, raw: Matrix):
+    def __init__(self):
         """Unfolds the gamma-detector response of a spectrum
 
         Args:
@@ -81,44 +82,37 @@ class Unfolder:
         TODO:
             - Fix the compton subtraction method implementation.
         """
-        self.raw: Matrix = raw
-        self.num_iter = 33
-        self.Ex_min = None
-        self.Ex_max = None
-        self.Eg_min = None
-        self.Eg_max = None
+        self.num_iter: int = 33
 
-        # Ensure that the given matrix is in fact raw
-        if self.raw.state != MatrixState.RAW:
-            warnings.warn("Trying to unfold matrix that is not raw")
+        # Used by properties to update Ex and Eg
+        self._Ex_min: float = None
+        self._Ex_max: float = None
+        self._Eg_min: float = None
+        self._Eg_max: float = None
 
     def update_values(self):
         """Verify internal consistency and set default values
 
         """
-        eps = 1e-3
+        # Ensure that the given matrix is in fact raw
+        if self.raw.state != MatrixState.RAW:
+            warnings.warn("Trying to unfold matrix that is not raw")
+
         LOG.debug("Comparing calibration of raw against response:"
                   f"\n{self.raw.calibration()}"
                   f"\n{self.R_calibration_array}")
         calibration_diff = self.raw.calibration_array() -\
             self.R_calibration_array
+        eps = 1e-3
         if not (np.abs(calibration_diff[2:]) < eps).all():
             raise AssertionError(("Calibration mismatch: "
                                   f"{calibration_diff}"
-                                  "Ensure that the raw matrix and"
+                                  "\nEnsure that the raw matrix and"
                                   " calibration matrix are cut equally."))
 
         # If energy limits are not provided, use extremal array values:
-        if self.Ex_min is None:
-            self.Ex_min = self.raw.Ex[0]
-        if self.Ex_max is None:
-            self.Ex_max = self.raw.Ex[-1]
-        if self.Eg_min is None:
-            self.Eg_min = self.raw.Eg[0]
-        if self.Eg_max is None:
-            self.Eg_max = self.raw.Eg[-1]
-        if self.mask is None:
-            raise ValueError("Make the mask before unfolding")
+        if self.mask_E1 is None:
+            raise ValueError("Call cut_diagonal() before unfolding")
 
         LOG.debug(("Using energy values:"
                    f"\nEg_min: {self.Eg_min}"
@@ -134,8 +128,11 @@ class Unfolder:
         iEg_min, iEg_max = 0, self.raw.index_Eg(self.Eg_max)
 
         # Create slices and cut the matrices to appropriate shape
-        Egslice = slice(iEg_min, iEg_max)
-        Exslice = slice(iEx_min, iEx_max)
+        # +1 as the slice goes up to but not including
+        Egslice = slice(iEg_min, iEg_max+1)
+        Exslice = slice(iEx_min, iEx_max+1)
+
+        self.mask = self.raw.line_mask(self.mask_E1, self.mask_E2)
 
         self.r = self.raw.values[Exslice, Egslice]
         self.mask = self.mask[Exslice, Egslice]
@@ -155,15 +152,17 @@ class Unfolder:
         self.weight_fluctuation = 0.2
         self.minimum_iterations = 3
 
-    def make_mask(self, E1: Iterable[float], E2: Iterable[float]):
-        self.mask = self.raw.line_mask(E1, E2)
+    def cut_diagonal(self, E1: Iterable[float], E2: Iterable[float]):
+        self.mask_E1 = E1
+        self.mask_E2 = E2
 
-    def unfold(self) -> np.ndarray:
+    def unfold(self, raw: Matrix) -> np.ndarray:
         """Run unfolding
 
         TODO: Use better criteria for terminating
         """
 
+        self.raw = copy(raw)
         # Set up the arrays
         self.update_values()
         unfolded_cube = np.zeros((self.num_iter, *self.r.shape))
@@ -181,7 +180,7 @@ class Unfolder:
 
             if LOG.level >= logging.DEBUG:
                 chisq = np.mean(chisquare[i, :])
-            LOG.debug(f"Iteration {i}: Avg χ²/ν {chisq}")
+                LOG.debug(f"Iteration {i}: Avg χ²/ν {chisq}")
 
         # Score the solutions based on χ² value for each Ex bin
         # and select the best one.
@@ -259,3 +258,35 @@ class Unfolder:
             self.minimum_iterations * np.ones(len(best_iteration), dtype=int),
             best_iteration)
         return best_iteration
+
+    @property
+    def Ex_min(self):
+        return self._Ex_min if self._Ex_min is not None else self.raw.Ex[0]
+
+    @Ex_min.setter
+    def Ex_min(self, value: float):
+        self._Ex_min = value
+
+    @property
+    def Ex_max(self):
+        return self._Ex_max if self._Ex_max is not None else self.raw.Ex[-1]
+
+    @Ex_max.setter
+    def Ex_max(self, value: float):
+        self._Ex_max = value
+
+    @property
+    def Eg_min(self):
+        return self._Eg_min if self._Eg_min is not None else self.raw.Eg[0]
+
+    @Eg_min.setter
+    def Eg_min(self, value: float):
+        self._Eg_min = value
+
+    @property
+    def Eg_max(self):
+        return self._Eg_max if self._Eg_max is not None else self.raw.Eg[-1]
+
+    @Eg_max.setter
+    def Eg_max(self, value: float):
+        self._Eg_max = value
