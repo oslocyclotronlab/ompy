ae6409951363cfab78ec18295772186c4553351e
Jørgen Eriksson Midtbø <jorgenem@gmail.com>
2019-04-29 11:35:50 +0200

    Renamed folder from oslo_method_python to ompy Updated import statements in notebooks

--- setup.py
+++ setup.py
@@ -5,15 +5,16 @@ from Cython.Build import cythonize
 # build me (i.e. compile Cython modules) for testing in this directory using
 # python setup.py build_ext --inplace
 
-setup(name='oslo_method_python',
-      version='0.1',
-      author="Jørgen Eriksson Midtbø",
-      author_email="jorgenem@gmail.com",
-      url="https://github.com/jorgenem/oslo_method_python",
-      py_modules=['oslo_method_python'],
+setup(name='OMpy',
+      version='0.2',
+      author="Jørgen Eriksson Midtbø, Fabio Zeiser, Erlend Lima",
+      author_email="jorgenem@gmail.com, fabio.zeiser@fys.uio.no,\
+                    erlendlima@outlook.com",
+      url="https://github.com/oslocyclotronlab/ompy",
+      py_modules=['ompy'],
       ext_modules=cythonize(
                             [
-                            "oslo_method_python/rebin.pyx",
-                            "oslo_method_python/rhosig.pyx"
+                            "ompy/rebin.pyx",
+                            "ompy/rhosig.pyx"
                             ])
       )
