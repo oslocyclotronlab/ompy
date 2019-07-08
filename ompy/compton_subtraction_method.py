e1ec48a7fbb97098d8119aa1449a37f76e7dbae6
Jørgen Eriksson Midtbø <jorgenem@gmail.com>
2019-07-08 12:25:26 +0200

    Added one more small function, shift(), which uses the same energy array for both input and output, i.e. it only shifts.

--- ompy/compton_subtraction_method.py
+++ ompy/compton_subtraction_method.py
@@ -23,3 +23,23 @@ def rebin_and_shift(counts_in, E_array_in, E_array_out, energy_shift=0):
     E_array_in_shifted = E_array_in + energy_shift
     counts_out = rebin(counts_in, E_array_in_shifted, E_array_out)
     return counts_out
+
+
+def shift(counts_in, E_array_in, energy_shift=0):
+    """
+    Shift the counts_in array by amount energy_shift.
+
+    The function is actually a wrapper for the rebin() function that
+    "fakes" the input energy calibration to give a shift. It is similar to
+    the rebin_and_shift() function defined above, but even simpler.
+
+    Args:
+        counts_in (numpy array, float): Array of counts
+        E_array_in (numpy array, float): Energy calibration (lower bin edge)
+                                         of input counts
+        energy_shift (float): Amount to shift the counts by. Negative means
+                              shift to lower energies. Default is 0.
+    """
+    E_array_in_shifted = E_array_in + energy_shift
+    counts_out = rebin(counts_in, E_array_in_shifted, E_array_in)
+    return counts_out
