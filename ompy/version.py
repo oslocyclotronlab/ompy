0e1be0d1b3056c28dabb9ca6bd24a710ffcacac4
Fabio Zeiser <fzeiser@users.noreply.github.com>
2019-10-10 16:57:24 +0200

    Delete version.py
    This should never have been here (on the repo), but shall be autogenerated

--- ompy/version.py
+++
@@ -1,7 +0,0 @@
-
-# THIS FILE IS GENERATED FROM OMPY SETUP.PY
-#
-short_version = '0.4.0'
-version = '0.4.0'
-full_version = '0.4.0.dev0+e0a322b'
-git_revision = 'e0a322b23e401abc86ed17e49e549382d9a05e55'
