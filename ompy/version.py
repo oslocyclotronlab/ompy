1cad1653aa96acbdd422cd5480615722504e2e5a
Vetle Wegner Ingeberg <vetlewi@fys.uio.no>
2019-10-10 13:54:34 +0200

    Removed sudo in the pip commands

--- ompy/version.py
+++ ompy/version.py
@@ -3,5 +3,5 @@
 #
 short_version = '0.4.0'
 version = '0.4.0'
-full_version = '0.4.0.dev0+2c0fbda'
-git_revision = '2c0fbdac1bdbcefdc7d1d3dde24d61127e1a8e4a'
+full_version = '0.4.0.dev0+e0a322b'
+git_revision = 'e0a322b23e401abc86ed17e49e549382d9a05e55'
