oslocyclotronlab/ompy

commit 06a81457152c1afd6b80ee2e92903f9fea28306c

Start better calibration match check + check for fill negatives (#63)
- Calibration match now bin by bin.
- Check for negative entries in ensemble generation, unfolding, firstgen and decompose
- Notebook at ensemble generation **seemed** to have a longer execution time, but that was only due to the `regenerate=True` flag. It takes just as long as before.
- small fixes

ompy/matrix.py
@@ -270,6 +270,7 @@ class Matrix():
             vmax Maximum value for coloring in scaling
             xlabel (optional, str): Label on x-axis. Default see source.
             ylabel (optional, str): Label on y-axis. Default see source.
+            kwargs: Additional kwargs to plot command.
         Returns:
             The ax used for plotting
         Raises:
@@ -351,6 +352,7 @@ class Matrix():
             normalize: Whether or not to normalize the counts.
             xlabel (optional, str): Label on x-axis. See source.
             ylabel (optional, str): Label on y-axis. Default is `None`.
+            kwargs: Additional kwargs to plot command.
         Raises:
             ValueError: If axis is not in [0, 1]
         Returns:
@@ -714,9 +716,9 @@ class Matrix():
     def remove_negative(self):
         self.values = np.where(self.values > 0, self.values, 0)
 
-    def fill_and_remove_negative(self):
+    def fill_and_remove_negative(self, window_size):
         """Temporary function to remove boilerplate"""
-        self.fill_negative(window_size=10)
+        self.fill_negative(window_size=window_size)
         self.remove_negative()
 
     def index_Eg(self, E: float) -> int:
@@ -798,20 +800,16 @@ class Matrix():
         return self.values.__setitem__(key, item)
 
     def __sub__(self, other) -> Matrix:
-        if not isinstance(other, Matrix):
-            raise TypeError("Other must be a Matrix")
-        if np.all(self.Ex != other.Ex) or np.all(self.Eg != other.Eg):
-            raise NotImplementedError()
-            # other = other.rebin('Ex', self.Ex, inplace=False)
-            # other = other.rebin('Eg', self.Eg, inplace=False)
+        self.has_equal_binning(other)
         result = copy.deepcopy(self)
         result.values -= other.values
         return result
 
     def __add__(self, other) -> Matrix:
-        if not isinstance(other, Matrix):
-            raise TypeError("Other must be a Matrix")
-        raise NotImplementedError
+        self.has_equal_binning(other)
+        result = copy.deepcopy(self)
+        result.values -= other.values
+        return result
 
     def __rmul__(self, factor) -> Matrix:
         other = copy.deepcopy(self)
@@ -820,6 +818,28 @@ class Matrix():
 
     def __mul__(self, factor) -> Matrix:
         return self.__rmul__(factor)
+
+    def has_equal_binning(self, other, **kwargs) -> bool:
+        """ Check whether `other` has equal binning as `self` within precision.
+        Args:
+            other (Matrix): Matrix to compare to.
+            kwargs: Additional kwargs to `np.allclose`.
+        Returns:
+            bool (bool): Returns `True` if both arrays are equal  .
+        Raises:
+            TypeError: If other is not a Matrix
+            ValueError: If any of the bins in any of the arrays are not equal.
+
+        """
+        if not isinstance(other, Matrix):
+            raise TypeError("Other must be a Matrix")
+        if np.any(self.shape != other.shape):
+            raise ValueError("Must have equal number of energy bins.")
+        if not np.allclose(self.Ex, other.Ex, **kwargs) \
+           or not np.allclose(self.Eg, other.Eg, **kwargs):
+            raise ValueError("Must have equal energy binning.")
+        else:
+            return True
 
 
 def to_plot_axis(axis: Any) -> int:

ompy/vector.py
@@ -97,6 +97,7 @@ class Vector():
                 `symlog` or `logit`.
             xlabel (optional, str): Label on x-axis. Default is `"Energy"`.
             ylabel (optional, str): Label on y-axis. Default is `None`.
+            kwargs: Additional kwargs to plot command.
         Returns:
             The figure and axis used.
         """
@@ -195,10 +196,12 @@ class Vector():
                 as itself.
         """
         # Hack since something is screwy with the import
-        if hasattr(other, 'E'):
-            if np.all(other.E != self.E):
-                raise ValueError("Must have equal energy binning")
+        # |-> is this comment still up to date?
+        try:
+            self.has_equal_binning(other)
             other = other.values
+        except TypeError:  # already an array
+            pass
         squared_error = (self.values - other)**2
         if self.std is not None:
             if std is not None:
@@ -268,11 +271,35 @@ class Vector():
         else:
             raise NotImplementedError("Uniits must be keV or MeV")
 
+    def has_equal_binning(self, other, **kwargs) -> bool:
+        """ Check whether `other` has equal_binning as `self` within precision.
+        Args:
+            other (Vector): Vector to compare to.
+            kwargs: Additional kwargs to `np.allclose`.
+        Returns:
+            bool (bool): Returns `True` if both arrays are equal  .
+        Raises:
+            TypeError: If other is not a Vector.
+            ValueError: If any of the bins in any of the arrays are not equal.
+
+        """
+        if not isinstance(other, Vector):
+            raise TypeError("Other must be a Vector")
+        if self.shape != other.shape:
+            raise ValueError("Must have equal number of energy bins.")
+        if not np.allclose(self.E, other.E, **kwargs):
+            raise ValueError("Must have equal energy binning.")
+        else:
+            return True
 
     def copy(self) -> Vector:
-        """ Return a copy of the matrix """
+        """ Return a copy of the Vector """
         return copy.deepcopy(self)
 
     def index(self, E) -> int:
         """ Returns the closest index corresponding to the E value """
         return index(self.E, E)
+
+    @property
+    def shape(self) -> Tuple[int]:
+        return self.values.shape

ompy/ensemble.py
@@ -233,6 +233,10 @@ class Ensemble:
         path = self.path / f"raw_{step}.npy"
         raw = self.load_matrix(path)
         if raw is None:
+            # if np.any(self.raw.values < 0):
+            #     raise ValueError("input matrix has to have positive"
+            #                      "entries only. Consider using fill and or"
+            #                      "remove negatives")
             LOG.debug(f"(Re)generating {path} using {method} process")
             if method == 'gaussian':
                 values = self.generate_gaussian()

ompy/extractor.py
@@ -184,7 +184,11 @@ class Extractor:
             Implement automatic rebinning if bin_width is given(?)
 
         """
+        if np.any(matrix.values < 0):
+            raise ValueError("input matrix has to have positive entries only.")
         if std is not None:
+            if np.any(std.values < 0):
+                raise ValueError("std has to have positive entries only.")
             assert matrix.shape == std.shape
             std.values = std.values.copy(order='C')
             matrix.values, std.values = normalize(matrix, std)

ompy/firstgeneration.py
@@ -121,6 +121,8 @@ class FirstGeneration:
         self.action.act_on(matrix)
         # We don't want negative energies
         matrix.cut('Ex', Emin=0.0)
+        if np.any(matrix.values < 0):
+            raise ValueError("input matrix has to have positive entries only.")
 
         valley_correction = self.cut_valley_correction(matrix)
 
@@ -324,8 +326,9 @@ class FirstGeneration:
             raise TypeError("`valley_correction` must be a vector.")
         valley_correction.copy()
         valley_correction.cut(Emin=matrix.Ex.min(), Emax=matrix.Ex.max())
-        assert (valley_correction.E == matrix.Ex).all(), \
-            "valley correction matrix must have the same Ex array as matrix."
+        valley_correction.has_equal_binning(matrix)
+        if np.any(valley_correction.values < 0):
+            raise ValueError("valley correction has to have positive entries only.")
 
         return valley_correction.values
 

ompy/normalizer.py
@@ -14,6 +14,7 @@ from numpy import ndarray
 from scipy.optimize import differential_evolution
 from typing import Optional, Sequence, Tuple, Any, Union, Callable, Dict, List
 from scipy.stats import truncnorm
+from tqdm import tqdm
 from .extractor import Extractor
 from .vector import Vector
 from .multinest_setup import run_nld_2regions
@@ -200,7 +201,7 @@ class Normalizer:
             nld = self.nld
             nlds = [nld]
 
-        for i, nld in enumerate(nlds):
+        for i, nld in enumerate(tqdm(nlds)):
             LOG.info(f"\n\n---------\nNormalizing nld #{i}")
             nld = nld.copy()
             LOG.debug("Setting NLD, convert to MeV")
@@ -395,7 +396,7 @@ class Normalizer:
                                 D0=self.nld_parameters['D0'][0],
                                 E=self.spin['Sn'])
         # TODO: plot errorbar
-        ax.scatter(self.spin['Sn'], Sn, label='$S_n$')
+        ax.scatter(self.spin['Sn'], Sn, label=r'$\rho(S_n)$')
 
         x = np.linspace(self.limit_high[0], self.spin['Sn'])
         model = self.curried_model(T=self.nld_parameters['T'][0],
@@ -409,6 +410,9 @@ class Normalizer:
                    alpha=0.1)
 
         ax.set_yscale('log')
+        ax.set_ylabel(r"$\rho(E_x) \quad [\mathrm{MeV}^{-1}]$")
+        ax.set_xlabel(r"E_x \quad [\mathrm{MeV}]$")
+
 
         if fig is not None:
             fig.legend(loc=9, ncol=3, frameon=False)

ompy/unfolder.py
@@ -75,6 +75,7 @@ class Unfolder:
         R (Matrix): The response matrix
         weight_fluctuation (float):
         minimum_iterations (int):
+        window_size (float or int?): window_size for fill negatives on output
         use_compton_subtraction (bool): Set usage of Compton subtraction method
         response_tab (DataFrame, optional): If `use_compton_subtraction=True`
             a table with information ('E', 'eff_tot', ...) must be provided.
@@ -99,6 +100,7 @@ class Unfolder:
         self.num_iter = num_iter
         self.weight_fluctuation = 0.2
         self.minimum_iterations = 3
+        self.window_size = 10
         self.zeroes: Optional[np.ndarray] = None
         self._param = 5
         self._R: Optional[Matrix] = response
@@ -106,7 +108,6 @@ class Unfolder:
         self.use_compton_subtraction: bool = True
         self.response_tab: Optional[pandas.DataFrame] = None
         self.FWHM_tweak_multiplier = None
-
 
     def __call__(self, matrix: Matrix) -> Matrix:
         """ Wrapper for self.apply() """
@@ -116,8 +117,10 @@ class Unfolder:
         """Verify internal consistency and set default values
 
         Raises:
-            AssertionError: If the raw matrix and response matrix
-                have different calibration coefficients a10 and a11.
+            ValueError: If the raw matrix and response matrix have different
+                calibrations.
+
+        Deleted Raises:
             AttributeError: If no diagonal cut has been made.
         """
         # Ensure that the given matrix is in fact raw
@@ -125,18 +128,28 @@ class Unfolder:
             warnings.warn("Trying to unfold matrix that is not raw")
 
         assert self.R is not None, "Response R must be set"
+        assert self.R.shape[0] == self.R.shape[1],\
+            "Response R must be a square matrix"
 
-        LOG.debug("Comparing calibration of raw against response:"
-                  f"\n{self.raw.calibration()}"
-                  f"\n{self.R.calibration()}")
-        calibration_diff = self.raw.calibration_array() -\
-            self.R.calibration_array()
-        eps = 1e-3
-        if not (np.abs(calibration_diff[2:]) < eps).all():
-            raise AssertionError(("Calibration mismatch: "
-                                  f"{calibration_diff}"
-                                  "\nEnsure that the raw matrix and"
-                                  " calibration matrix are cut equally."))
+        LOG.debug("Comparing calibration of raw against response")
+        if len(self.raw.Eg) != len(self.R.Eg):
+            raise ValueError("Must have equal number of energy bins.")
+        if not np.allclose(self.raw.Eg, self.R.Eg):
+            raise ValueError("Must have equal energy binning.")
+
+        LOG.debug("Check for negative counts.")
+        if np.any(self.raw.values < 0) or np.any(self.R.values < 0):
+            raise ValueError("Raw and response cannot have negative counts."
+                             "Consider using fill_negatives and "
+                             "remove_negatives on the input matixes.")
+        # calibration_diff = self.raw.calibration_array() -\
+        #     self.R.calibration_array()
+        # eps = 1e-3
+        # if not (np.abs(calibration_diff[2:]) < eps).all():
+        #     raise AssertionError(("Calibration mismatch: "
+        #                           f"{calibration_diff}"
+        #                           "\nEnsure that the raw matrix and"
+        #                           " calibration matrix are cut equally."))
 
         # TODO: Warn if the Matrix is not diagonal
         # raise AttributeError("Call cut_diagonal() before unfolding")
@@ -198,8 +211,7 @@ class Unfolder:
         # These two lines feel out of place
         # TODO: What they do and where they should be run is very unclear.
         #     Fix later.
-        unfolded.fill_negative(window_size=10)
-        unfolded.remove_negative()
+        unfolded.fill_and_remove_negative(window_size=self.window_size)
         return unfolded
 
     def step(self, unfolded, folded, step):

commit 1b8c95dec3889cc53e2e8583ea57405d9582917d

Implement more + fix numeric operations for Matrix and Vector
Implement more operations of the type a+b, a/b ... for matrix and
vector. Also implemented tests to check them.
Fixed a copy&paste bug in __add__ introduced in 06a81457

ompy/matrix.py
@@ -205,7 +205,7 @@ class Matrix():
             self.values, self.Eg, self.Ex = mama_read(path)
         else:
             try:
-                self.matrix, self.Eg, self.Ex = mama_read(path)
+                self.values, self.Eg, self.Ex = mama_read(path)
             except ValueError:  # from within mama_read
                 raise ValueError(f"Unknown filetype {filetype}")
         self.verify_integrity()
@@ -822,24 +822,64 @@ class Matrix():
         return self.values.__setitem__(key, item)
 
     def __sub__(self, other) -> Matrix:
-        self.has_equal_binning(other)
         result = copy.deepcopy(self)
-        result.values -= other.values
+        if isinstance(other, (int, float)):
+            result.values -= other
+        else:
+            self.has_equal_binning(other)
+            result.values -= other.values
+        return result
+
+    def __rsub__(self, other) -> Matrix:
+        result = copy.deepcopy(self)
+        if isinstance(other, (int, float)):
+            result.values = other - result.values
+        else:
+            self.has_equal_binning(other)
+            result.values = other.values - result.values
         return result
 
     def __add__(self, other) -> Matrix:
-        self.has_equal_binning(other)
         result = copy.deepcopy(self)
-        result.values -= other.values
+        if isinstance(other, (int, float)):
+            result.values += other
+        else:
+            self.has_equal_binning(other)
+            result.values += other.values
         return result
 
-    def __rmul__(self, factor) -> Matrix:
-        other = copy.deepcopy(self)
-        other.values *= factor
-        return other
+    def __radd__(self, other) -> Matrix:
+        return self.__add__(other)
 
-    def __mul__(self, factor) -> Matrix:
-        return self.__rmul__(factor)
+    def __mul__(self, other) -> Matrix:
+        result = copy.deepcopy(self)
+        if isinstance(other, (int, float)):
+            result.values *= other
+        else:
+            self.has_equal_binning(other)
+            result.values *= other.values
+        return result
+
+    def __rmul__(self, other) -> Matrix:
+        return self.__mul__(other)
+
+    def __truediv__(self, other) -> Matrix:
+        result = copy.deepcopy(self)
+        if isinstance(other, (int, float)):
+            result.values /= other
+        else:
+            self.has_equal_binning(other)
+            result.values /= other.values
+        return result
+
+    def __rtruediv__(self, other) -> Matrix:
+        result = copy.deepcopy(self)
+        if isinstance(other, (int, float)):
+            result.values = other / result.values
+        else:
+            self.has_equal_binning(other)
+            result.values = other.values / result.values
+        return result
 
     def has_equal_binning(self, other, **kwargs) -> bool:
         """ Check whether `other` has equal binning as `self` within precision.

ompy/vector.py
@@ -312,3 +312,65 @@ class Vector():
     @property
     def shape(self) -> Tuple[int]:
         return self.values.shape
+
+    def __sub__(self, other) -> Vector:
+        result = copy.deepcopy(self)
+        if isinstance(other, (int, float)):
+            result.values -= other
+        else:
+            self.has_equal_binning(other)
+            result.values -= other.values
+        return result
+
+    def __rsub__(self, other) -> Vector:
+        result = copy.deepcopy(self)
+        if isinstance(other, (int, float)):
+            result.values = other - result.values
+        else:
+            self.has_equal_binning(other)
+            result.values = other.values - result.values
+        return result
+
+    def __add__(self, other) -> Vector:
+        result = copy.deepcopy(self)
+        if isinstance(other, (int, float)):
+            result.values += other
+        else:
+            self.has_equal_binning(other)
+            result.values += other.values
+        return result
+
+    def __radd__(self, other) -> Vector:
+        return self.__add__(other)
+
+    def __mul__(self, other) -> Vector:
+        result = copy.deepcopy(self)
+        if isinstance(other, (int, float)):
+            result.values *= other
+        else:
+            self.has_equal_binning(other)
+            result.values *= other.values
+        return result
+
+    def __rmul__(self, other) -> Vector:
+        return self.__mul__(other)
+
+    def __truediv__(self, other) -> Vector:
+        result = copy.deepcopy(self)
+        if isinstance(other, (int, float)):
+            result.values /= other
+        else:
+            self.has_equal_binning(other)
+            result.values /= other.values
+        return result
+
+    def __rtruediv__(self, other) -> Vector:
+        result = copy.deepcopy(self)
+        if isinstance(other, (int, float)):
+            result.values = other / result.values
+        else:
+            self.has_equal_binning(other)
+            result.values = other.values / result.values
+        return result
+
+

tests/test_matrix.py
@@ -1,6 +1,7 @@
 import pytest
 import ompy as om
 import numpy as np
+from numpy.testing import assert_equal
 
 
 @pytest.fixture()
@@ -58,3 +59,34 @@ def test_index(E, index):
     mat.Ex = np.arange(-10.5, 10.5)
     assert mat.index_Ex(E) == index
 
+
+@pytest.mark.filterwarnings('ignore:divide by zero encountered in true_divide:RuntimeWarning')
+def test_numericals():
+    E = np.array([0, 1, 2])
+    values1 = np.array([[0, 1, 2.], [-2, 1, 2.],  [2, 3, -10.]])
+    matrix1 = om.Matrix(values=values1, Ex=E, Eg=E)
+
+    values2 = values1+1
+    matrix2 = om.Matrix(values=values2, Ex=E, Eg=E)
+
+    factor = 5.
+
+    assert_equal((matrix1/matrix2).values, values1/values2)
+    assert_equal((matrix2/matrix1).values, values2/values1)
+    assert_equal((matrix1/factor).values, values1/factor)
+    assert_equal((factor/matrix1).values, factor/values1)
+
+    assert_equal((matrix1*matrix2).values, values1*values2)
+    assert_equal((matrix2*matrix1).values, values2*values1)
+    assert_equal((matrix1*factor).values, values1*factor)
+    assert_equal((factor*matrix1).values, factor*values1)
+
+    assert_equal((matrix1+matrix2).values, values1+values2)
+    assert_equal((matrix2+matrix1).values, values2+values1)
+    assert_equal((matrix1+factor).values, values1+factor)
+    assert_equal((factor+matrix1).values, factor+values1)
+
+    assert_equal((matrix1-matrix2).values, values1-values2)
+    assert_equal((matrix2-matrix1).values, values2-values1)
+    assert_equal((matrix1-factor).values, values1-factor)
+    assert_equal((factor-matrix1).values, factor-values1)

tests/test_vector.py
@@ -35,3 +35,34 @@ def test_cut():
     valcut = np.arange(34, 43)
     assert_equal(vector.E, Ecut)
     assert_equal(vector.values, valcut)
+
+@pytest.mark.filterwarnings('ignore:divide by zero encountered in true_divide:RuntimeWarning')
+def test_numericals():
+    E = np.array([0, 1, 2])
+    values1 = np.array([0, 1, -2.])
+    vector1 = om.Vector(values=values1, E=E)
+
+    values2 = values1+1
+    vector2 = om.Vector(values=values2, E=E)
+
+    factor = 5.
+
+    assert_equal((vector1/vector2).values, values1/values2)
+    assert_equal((vector2/vector1).values, values2/values1)
+    assert_equal((vector1/factor).values, values1/factor)
+    assert_equal((factor/vector1).values, factor/values1)
+
+    assert_equal((vector1*vector2).values, values1*values2)
+    assert_equal((vector2*vector1).values, values2*values1)
+    assert_equal((vector1*factor).values, values1*factor)
+    assert_equal((factor*vector1).values, factor*values1)
+
+    assert_equal((vector1+vector2).values, values1+values2)
+    assert_equal((vector2+vector1).values, values2+values1)
+    assert_equal((vector1+factor).values, values1+factor)
+    assert_equal((factor+vector1).values, factor+values1)
+
+    assert_equal((vector1-vector2).values, values1-values2)
+    assert_equal((vector2-vector1).values, values2-values1)
+    assert_equal((vector1-factor).values, values1-factor)
+    assert_equal((factor-vector1).values, factor-values1)