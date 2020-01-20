0a3f4c467b9b30e5f3d73b8ca0df23b97d6fc769
fzeiser <fzeiser@physnet.uni-hamburg.de>
2020-01-20 19:15:11 +0100

    fixed tests after last commit

--- tests/test_firstgen.py
+++ tests/test_firstgen.py
@@ -18,9 +18,9 @@ def test_multiplicity_estimation(firstgen):
 
 @pytest.mark.parametrize(
         "Ex,fg_Ex",
-        [(np.arange(-1.0, 4.0), np.arange(0.0, 4.0)),
-         (np.arange(-1.2, 2.9, 1), np.arange(0.8, 2.9, 1))])
-def test_cutoff(firstgen, Ex, fg_Ex):
+        [(np.arange(0.0, 5.0), np.arange(0.0, 5.0)),
+         (np.arange(0.2, 5.2, 1), np.arange(0.2, 5.2, 1))])
+def test_shape(firstgen, Ex, fg_Ex):
     values = np.ones((5, 5))
     values = np.tril(values)
     mat = om.Matrix(values=values)
@@ -29,3 +29,17 @@ def test_cutoff(firstgen, Ex, fg_Ex):
     first = firstgen.apply(mat)
     assert first.Ex.shape == fg_Ex.shape
     np.testing.assert_allclose(first.Ex, fg_Ex)
+
+
+@pytest.mark.parametrize(
+        "Ex,fg_Ex",
+        [(np.arange(-1.0, 4.0), np.arange(0.0, 4.0)),
+         (np.arange(-1.2, 2.9, 1), np.arange(0.8, 2.9, 1))])
+def test_negatives(firstgen, Ex, fg_Ex):
+    values = np.ones((5, 5))
+    values = np.tril(values)
+    mat = om.Matrix(values=values)
+    mat.Ex = Ex
+    mat.verify_integrity()
+    with pytest.raises(ValueError):
+        firstgen.apply(mat)
