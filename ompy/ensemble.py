oslocyclotronlab/ompy

commit e86a96031bfa6c9e94122df4043a8a0cb84f8de7

Small fixes related to plotting (titles, colors, ...)

ompy/ensemble.py
@@ -146,14 +146,12 @@ class Ensemble:
         self.seed: int = 987654
         self.nprocesses: int = cpu_count()-1 if cpu_count() > 1 else 1
 
-        if path is not None:
-            self.path = Path(path)
-            self.path.mkdir(exist_ok=True)
+        self.path = Path(path) if path is not None else Path('ensemble')
+        self.path.mkdir(exist_ok=True)
+        try:
             self.load()
-        else:
-            # Should the ensemble always try to load?
-            self.path = Path('ensemble')
-            self.path.mkdir(exist_ok=True)
+        except FileNotFoundError:
+            pass
 
         self.raw.state = "raw"
 
@@ -632,7 +630,8 @@ class Ensemble:
                 raise ValueError("Three axes must be provided")
             fig = ax.figure
         else:
-            fig, ax = plt.subplots(ncols=3, sharey=True, constrained_layout=True)
+            fig, ax = plt.subplots(ncols=3, sharey=True,
+                                   constrained_layout=True)
 
         extrema = lambda x: (np.min(x), np.max(x)) # noqa
         choices = {"raw": extrema(self.std_raw.values),
@@ -658,7 +657,8 @@ class Ensemble:
         self.std_unfolded.plot(ax=ax[1], title='Unfolded', add_cbar=False,
                                vmin=vmin, vmax=vmax, **kwargs)
         im, _, _ = self.std_firstgen.plot(ax=ax[2], title='First Generation',
-                               vmin=vmin, vmax=vmax, add_cbar=False, **kwargs)
+                                          vmin=vmin, vmax=vmax,
+                                          add_cbar=False, **kwargs)
 
         # Y labels only clutter
         ax[1].set_ylabel(None)

ompy/ensembleNormalizer.py
@@ -200,7 +200,7 @@ class EnsembleNormalizer:
         if ax is None:
             fig, ax = plt.subplots(1, 2, constrained_layout=True)
         else:
-            fig = ax.figure
+            fig = ax[0].figure
 
         norm_sim = self.normalizer_simultan
         if norm_sim is not None:
@@ -220,16 +220,18 @@ class EnsembleNormalizer:
                             n_plot=n_plot)
 
         # get median, 1 sigma, ...
+        prop_cycle = plt.rcParams['axes.prop_cycle']
+        colors = prop_cycle.by_key()['color']
         percentiles = [0.16, 0.84]
-        lines = self.plot_vector_stats(ax, samples, percentiles)
+        lines = self.plot_vector_stats(ax, samples, percentiles, colors[1])
 
         if plot_model_stats:
             Emin = samples["nld"].iloc[0].E[-1]
             x = np.linspace(Emin, normalizer_nld.norm_pars.Sn[0], num=20)
-            color = lines.get_facecolor()
             self.plot_nld_ext_stats(ax[0], x=x, samples=samples,
                                     normalizer_nld=normalizer_nld,
-                                    percentiles=percentiles, color=color)
+                                    percentiles=percentiles, color=colors[2],
+                                    label="model")
 
             E = samples["gsf"].iloc[0].E
             xlow = np.linspace(0.001, E[0], num=20)
@@ -237,7 +239,7 @@ class EnsembleNormalizer:
             self.plot_gsf_ext_stats(ax[1], xlow=xlow, xhigh=xhigh,
                                     samples=samples,
                                     normalizer_gsf=normalizer_gsf,
-                                    percentiles=percentiles, color=color)
+                                    percentiles=percentiles, color=colors[2])
 
         if add_figlegend:
             fig.legend(loc=9, ncol=4, frameon=True)
@@ -298,8 +300,11 @@ class EnsembleNormalizer:
         if random_state is None:  # cannot move this to definition
             random_state = np.random.RandomState(98765)
 
+        # dummy to draw axis
+        n_plot_ = n_plot if n_plot > 0 else 1
+
         res = copy.deepcopy(self.res[0])  # dummy for later
-        selection = samples.sample(n=n_plot, random_state=random_state)
+        selection = samples.sample(n=n_plot_, random_state=random_state)
         for i, (_, row) in enumerate(selection.iterrows()):
             res.nld = row["nld"]
             res.gsf = row["gsf"]
@@ -321,13 +326,21 @@ class EnsembleNormalizer:
             add_label = True if i == 0 else False
             plot_fitregion = True if i == 0 else False
             normalizer_nld.plot(ax=ax[0], results=res,
-                                add_label=add_label, alpha=1/n_plot,
+                                add_label=add_label, alpha=1/n_plot_,
                                 add_figlegend=False,
                                 plot_fitregion=plot_fitregion)
             normalizer_gsf.plot(ax=ax[1], results=res, add_label=False,
-                                alpha=1/n_plot,
+                                alpha=1/n_plot_,
                                 add_figlegend=False,
                                 plot_fitregion=plot_fitregion)
+
+        if n_plot == 0:  # remove lines if dummy only
+            l_keep = []
+            for l in ax[0].lines:
+                if l._label in ["known levels", "_nld(Sn)"]:
+                    l_keep.append(l)
+            ax[0].lines = [*l_keep]
+            ax[1].lines = []
 
     @staticmethod
     def stats_from_df(df, fmap, shape_out, percentiles):
@@ -343,11 +356,13 @@ class EnsembleNormalizer:
         return stats
 
     @staticmethod
-    def plot_vector_stats(ax: Tuple[Any, Any], samples, percentiles):
+    def plot_vector_stats(ax: Tuple[Any, Any], samples, percentiles, color):
 
-        # workaround as dataframe changes limits
+        # workaround as dataframe changes limits & labels
         lim_ax0 = [ax[0].get_xlim(), ax[0].get_ylim()]
         lim_ax1 = [ax[1].get_xlim(), ax[1].get_ylim()]
+        label_ax0 = [ax[0].get_xlabel(), ax[0].get_ylabel()]
+        label_ax1 = [ax[1].get_xlabel(), ax[1].get_ylabel()]
 
         # define helper function
         def vec_to_values(x, out):  # noqa
@@ -360,7 +375,8 @@ class EnsembleNormalizer:
                                             shape_out=(len(df), len(E)),
                                             percentiles=percentiles)  # noqa
         stats_nld["x"] = E
-        stats_nld.plot(x="x", y="median", ax=ax[0], legend=False)
+        stats_nld.plot(x="x", y="median", ax=ax[0], legend=False,
+                       color=color)
 
         df = samples["gsf"]
         E = df.iloc[0].E
@@ -368,12 +384,14 @@ class EnsembleNormalizer:
                                             shape_out=(len(df), len(E)),
                                             percentiles=percentiles)  # noqa
         stats_gsf["x"] = df.iloc[0].E
-        stats_gsf.plot(x="x", y="median", ax=ax[1], legend=False)
+        stats_gsf.plot(x="x", y="median", ax=ax[1], legend=False,
+                       color=color)
 
         pc_diff = percentiles[1] - percentiles[0]
+        label = fr"{(pc_diff)*100:.0f}\% credibility interval"
         ax[0].fill_between(stats_nld.x, stats_nld["low"], stats_nld["high"],
                            alpha=0.3,
-                           label=f"{(pc_diff)*100:.0f}% credibility interval")
+                           label=label)
         lines = ax[1].fill_between(stats_gsf.x, stats_gsf["low"],
                                    stats_gsf["high"],
                                    alpha=0.3)
@@ -382,25 +400,29 @@ class EnsembleNormalizer:
         ax[0].set_ylim(lim_ax0[1])
         ax[1].set_xlim(lim_ax1[0])
         ax[1].set_ylim(lim_ax1[1])
+
+        ax[0].set_xlabel(label_ax0[0])
+        ax[0].set_ylabel(label_ax0[1])
+        ax[1].set_xlabel(label_ax1[0])
+        ax[1].set_ylabel(label_ax1[1])
+
         return lines
 
     @staticmethod
     def plot_nld_ext_stats(ax: Any, *, x: np.ndarray,
                            samples, normalizer_nld, percentiles,
-                           color):
+                           **kwargs):
         # define helper function
         def to_values(a, out):  # noqa
             idx, val = a
-            out[idx] = normalizer_nld.curried_model(T=val[0],
-                                                    D0=val[1],
-                                                    E=x)
+            out[idx] = normalizer_nld.model(E=x, T=val[0], Eshift=val[1])
 
-        df = samples[["T", "D0"]]
+        df = samples[["T", "Eshift"]]
         stats = EnsembleNormalizer.stats_from_df(df, fmap=to_values,
                                                  shape_out=(len(df), len(x)),
                                                  percentiles=percentiles)
         ax.fill_between(x, stats["low"], stats["high"],
-                        alpha=0.3, color=color)
+                        alpha=0.3, **kwargs)
 
     @staticmethod
     def plot_gsf_ext_stats(ax: Any, *, xlow: np.ndarray, xhigh: np.ndarray,

ompy/extractor.py
@@ -546,7 +546,7 @@ class Extractor:
                            alpha=1/len(self.gsf), **kwargs)
 
         ax[0].set_title("Level density")
-        ax[1].set_title("γSF")
+        ax[1].set_title(r"$\gamma$SF")
         if scale == 'log':
             ax[0].set_yscale("log")
             ax[1].set_yscale("log")

ompy/normalizer_gsf.py
@@ -430,6 +430,8 @@ class NormalizerGSF():
                        color='grey', alpha=0.1)
 
         ax.set_yscale('log')
+        ax.set_xlabel(rf"$\gamma$-ray energy $E_\gamma$~[{gsf.units}]")
+        ax.set_ylabel(rf"$\gamma$-SF f($E_\gamma$)~[${gsf.units}^{-3}$]")
 
         if fig is not None and add_figlegend:
             fig.legend(loc=9, ncol=3, frameon=False)
@@ -514,7 +516,10 @@ class NormalizerGSF():
 
 def fnld(E: ndarray, nld: Vector,
          nld_model: Callable) -> ndarray:
-    """ Function composed of nld and model, providing y = nld(E) """
+    """ Function composed of nld and model, providing y = nld(E)
+
+    It will take the extrapolation where no exp. data is available.
+    """
     fexp = log_interp1d(nld.E, nld.values)
 
     conds = [E <= nld.E[-1], E > nld.E[-1]]
@@ -523,7 +528,10 @@ def fnld(E: ndarray, nld: Vector,
 
 def fgsf(E: ndarray, gsf: Vector,
          gsf_low: Vector, gsf_high: Vector) -> ndarray:
-    """ Function composed of gsf and model, providing y = gsf(E) """
+    """ Function composed of gsf and model, providing y = gsf(E)
+
+    It will take the extrapolation where no exp. data is available.
+    """
     exp = log_interp1d(gsf.E, gsf.values)
     ext_low = log_interp1d(gsf_low.E, gsf_low.values)
     ext_high = log_interp1d(gsf_high.E, gsf_high.values)

ompy/normalizer_nld.py
@@ -415,9 +415,10 @@ class NormalizerNLD:
             labelNld = 'exp.'
             labelNldSn = r'$\rho(S_n)$'
             labelModel = 'model'
+            labelDiscrete = "known levels"
         nld.plot(ax=ax, label=labelNld, **kwargs)
 
-        self.discrete.plot(ax=ax, c='k', label=labelDiscrete)
+        self.discrete.plot(ax=ax, kind='step', c='k', label=labelDiscrete)
 
         nldSn = self.nldSn_from_D0(**self.norm_pars.asdict())[1]
         rel_uncertainty = self.norm_pars.D0[1]/self.norm_pars.D0[0]
@@ -430,6 +431,10 @@ class NormalizerNLD:
 
         ax.errorbar(self.norm_pars.Sn[0], nldSn[0], yerr=nldSn[1],
                     label=labelNldSn, fmt="s", markerfacecolor='none')
+        # workaround for enseble Normalizer; always keep these label
+        for i in range(3):
+            ax.lines[-(i+1)]._label = "_nld(Sn)"
+
         ax.plot(model.E, model.values, "--", label=labelModel, markersize=0,
                 c='g', **kwargs)
 
@@ -440,8 +445,8 @@ class NormalizerNLD:
                        alpha=0.1)
 
         ax.set_yscale('log')
-        ax.set_ylabel(r"$\rho(E_x) \quad [\mathrm{MeV}^{-1}]$")
-        ax.set_xlabel(r"$E_x \quad [\mathrm{MeV}]$")
+        ax.set_ylabel(r"Level density $\rho(E_x)~[\mathrm{MeV}^{-1}]$")
+        ax.set_xlabel(r"Excitation energy $E_x~[\mathrm{MeV}]$")
         ax.set_ylim(bottom=0.5/(nld.E[1]-nld.E[0]))
 
         if fig is not None and add_figlegend:

ompy/normalizer_simultan.py
@@ -369,7 +369,7 @@ class NormalizerSimultan():
         if ax is None:
             fig, ax = plt.subplots(1, 2, constrained_layout=True)
         else:
-            fig = ax.figure
+            fig = ax[0].figure
 
         self.normalizer_nld.plot(ax=ax[0], add_label=True, results=self.res,
                                  add_figlegend=False, **kwargs)
@@ -377,7 +377,7 @@ class NormalizerSimultan():
                                  add_figlegend=False, **kwargs)
 
         ax[0].set_title("Level density")
-        ax[1].set_title("γSF")
+        ax[1].set_title(r"$\gamma$SF")
 
         if add_figlegend:
             fig.legend(loc=9, ncol=4, frameon=True)

commit cef8ef8f4e1387344d71e989e91e728cea34086f

Fixing bug introduced in e86a960: Regenerate of Ensemble class
Ensemble class now always loaded the results, if results were saved.
This was not intended. The easiest way to fix this is to have
`load()` just as a separate possibility to `regenerate`.

ompy/ensemble.py
@@ -148,15 +148,11 @@ class Ensemble:
 
         self.path = Path(path) if path is not None else Path('ensemble')
         self.path.mkdir(exist_ok=True)
-        try:
-            self.load()
-        except FileNotFoundError:
-            pass
 
         self.raw.state = "raw"
 
     def load(self, path: Optional[Union[str, Path]] = None):
-        """ Loads a saved ensamble
+        """ Loads a saved ensamble. Alternative to `regenerate`.
 
         Currently only supports '.npy' format.
 