7aba41de98bde36154dfca88553bffc9b9e0c1a5
Vetle Wegner Ingeberg <vetlewi@fys.uio.no>
2020-11-03 15:51:02 +0100

    Added test for vectorized versions as well

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -9,7 +9,17 @@ def test_normal():
     N = 10000
     x = np.random.rand(N)
 
-    norm_native = np.array([om.stats.normal(xi) for xi in x])
+    norm_native = np.array([om.stats.normal_ppf(xi) for xi in x])
+    norm_scipy = stats.norm.ppf(x)
+
+    assert_allclose(norm_native, norm_scipy)
+
+
+def test_normal_vec():
+    N = 10000
+    x = np.random.rand(N)
+
+    norm_native = om.stats.normal_ppf_vec(x)
     norm_scipy = stats.norm.ppf(x)
 
     assert_allclose(norm_native, norm_scipy)
@@ -22,7 +32,37 @@ def test_truncnorm():
     a = -5.
     b = 25.
 
-    truncnorm_native = np.array([om.stats.truncnorm(xi, a, b) for xi in x])
+    truncnorm_native = np.array([om.stats.truncnorm_ppf(xi, a, b) for xi in x])
+    truncnorm_scipy = stats.truncnorm.ppf(x, a, b)
+
+    assert_allclose(truncnorm_native, truncnorm_scipy)
+
+
+def test_truncnorm_vec():
+    N = 10000
+    x = np.random.rand(N)
+
+    a = -5.
+    b = 25.
+
+    truncnorm_native = om.stats.truncnorm_ppf_vec(x, a, b)
+    truncnorm_scipy = stats.truncnorm.ppf(x, a, b)
+
+    assert_allclose(truncnorm_native, truncnorm_scipy)
+
+    a = np.array([-5., 5.])
+    b = np.array([25., 25.])
+
+    x = np.random.rand()
+
+    truncnorm_native = om.stats.truncnorm_ppf_vec(x, a, b)
+    truncnorm_scipy = stats.truncnorm.ppf(x, a, b)
+
+    assert_allclose(truncnorm_native, truncnorm_scipy)
+
+    x = np.random.rand(2)
+
+    truncnorm_native = om.stats.truncnorm_ppf_vec(x, a, b)
     truncnorm_scipy = stats.truncnorm.ppf(x, a, b)
 
     assert_allclose(truncnorm_native, truncnorm_scipy)
