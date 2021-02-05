effb7864ee05e5ba392c404c09862e3d3b9cfbfc
Vetle Wegner Ingeberg <vetlewi@fys.uio.no>
2021-02-05 12:34:14 +0100

    Last check
    One last check to make sure we dont need to account for old IPython versions

--- check_env.py
+++ check_env.py
@@ -10,22 +10,8 @@ import pathos
 import pybind11
 import watermark
 
-try:
-    from IPython import get_ipython
-    ipython = get_ipython()
-    ipython.magic("load_ext watermark")
-    ipython.magic("watermark -m -u -d -v -iv -w")
-except:
-    try:
-        import IPython.core.ipapi  
-        ipython = IPython.core.ipapi.get()
-        ipython.magic("load_ext watermark")
-        ipython.magic("watermark -m -u -d -v -iv -w")
-    except:
-        try:
-            import IPython.ipapi  
-            ipython = IPython.ipapi.get()
-            ipython.magic("load_ext watermark")
-            ipython.magic("watermark -m -u -d -v -iv -w")
-        except:
-            print("Unable to show versions")
+
+from IPython import get_ipython
+ipython = get_ipython()
+ipython.magic("load_ext watermark")
+ipython.magic("watermark -m -u -d -v -iv -w")
