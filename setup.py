7e92394ca893555098ca28372f99c7046afc823a
fzeiser <fzeiser@physnet.uni-hamburg.de>
2021-03-16 18:17:12 +0100

    small fix

--- setup.py
+++ setup.py
@@ -162,7 +162,7 @@ ext_modules_pybind11 = [
 
 install_requires = [
  "cython",
- "numpy>=1.17",
+ "numpy>=1.17; sys_platform != 'darwin'",
  "numpy>=1.20; sys_platform == 'darwin'",
  "pandas",
  "matplotlib",
